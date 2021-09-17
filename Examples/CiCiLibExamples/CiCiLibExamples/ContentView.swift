8af400fa017941d622b5502082b18439f44a9cc7
Liu <bruce.liu@experian.com>
2021-09-16 19:25:25 -0700

    How to use Library

--- Examples/CiCiLibExamples/CiCiLibExamples/ContentView.swift
+++ Examples/CiCiLibExamples/CiCiLibExamples/ContentView.swift
@@ -2,17 +2,59 @@ import SwiftUI
 import CiCiLib
 
 struct ContentView: View {
-    var body: some View {
-        
-        HelloView()
-    }
-}
-
-func test() {
-    //var a = Stack<Int>()
     
-    var h = Hello(name: "Kevin")
-    h.print()
+    private let pair = Pair(first: 1, second: "One")
+    private let hello = Hello(name: "Jason")
+    private let stack = Stack<Int>()
+    
+    @State private var numbers = [Int]()
+    
+    var body: some View {
+        VStack {
+            HelloView(name: "Kevin")
+                .padding()
+
+            Text(pair.toString())
+                .padding()
+            
+            Text(hello.toString())
+                .padding()
+            
+            Button("Test Stack") {
+                testStack()
+            }
+            .padding()
+            
+            VStack(alignment: .leading) {
+                if !numbers.isEmpty {
+                    Text("Stack")
+                        .font(.title)
+                        .padding()
+                }
+                
+                List(numbers, id: \.self) { num in
+                    Text("\(num)")
+                }
+                .padding()
+            }
+        }
+    }
+    
+    func testStack() {
+        
+        let a = Stack<Int>()
+        
+        a.push(element: 1)
+        a.push(element: 2)
+        a.push(element: 3)
+        
+        while !a.isEmpty() {
+            let top = try! a.top()
+            numbers.append(top)
+            
+            try! a.pop()
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
